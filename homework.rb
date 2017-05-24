6e093e81ea2049264363aa53081135d530e6c618
Faisal <faisal_a@live.co.uk>
2017-05-24 23:47:08 +0100

    Completed hash manipulation questions

--- homework.rb
+++ homework.rb
@@ -43,7 +43,7 @@
 
 ### B. Given the following data structure:
 
-my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
+# my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
 
@@ -63,56 +63,74 @@ my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 5. How would you add `{:four => 4}` to the hash?
 
-my_hash[:four] = 4
+# my_hash[:four] = 4
 
-### C. Given the following data structure:
+# C. Given the following data structure:
 
-# users = {
-#   "Jonathan" => {
-#     :twitter => "jonnyt",
-#     :favourite_numbers => [12, 42, 75, 12, 5],
-#     :home_town => "Stirling",
-#     :pets => {
-#       "fluffy" => :cat,
-#       "fido" => :dog,
-#       "spike" => :dog
-#     }
-#   },
-#   "Erik" => {
-#     :twitter => "eriksf",
-#     :favourite_numbers => [8, 12, 24],
-#     :home_town => "Linlithgow",
-#     :pets => {
-#       "nemo" => :fish,
-#       "kevin" => :fish,
-#       "spike" => :dog,
-#       "rupert" => :parrot
-#     }
-#   },
-#   "Avril" => {
-#     :twitter => "bridgpally",
-#     :favourite_numbers => [12, 14, 85, 88],
-#     :home_town => "Dunbar",
-#     :pets => {
-#       "colin" => :snake
-#     }
-#   }
-# }
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :favourite_numbers => [12, 42, 75, 12, 5],
+    :home_town => "Stirling",
+    :pets => {
+      "fluffy" => :cat,
+      "fido" => :dog,
+      "spike" => :dog
+    }
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :favourite_numbers => [8, 12, 24],
+    :home_town => "Linlithgow",
+    :pets => {
+      "nemo" => :fish,
+      "kevin" => :fish,
+      "spike" => :dog,
+      "rupert" => :parrot
+    }
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :favourite_numbers => [12, 14, 85, 88],
+    :home_town => "Dunbar",
+    :pets => {
+      "colin" => :snake
+    }
+  }
+}
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 
+# puts users["Jonathan"][:twitter]
+
 # 2. Return Erik's hometown
+
+# puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
 
+ # print users["Erik"][:favourite_numbers] 
+
 # 4. Return the type of Avril's pet Colin
+
+# puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
 
+# puts users["Erik"][:favourite_numbers].min
+
 # 6. Add the number `7` to Erik's favorite numbers
+
+# users["Erik"][:favourite_numbers].push(3)
 
 # 7. Change Erik's hometown to Edinburgh
 
+# users["Erik"][:home_town] = "Edinburgh"
+
 # 8. Add a pet dog to Erik called "Fluffy"
 
+# users["Erik"][:pets]["Fluffy"] = :dog 
+
 # 9. Add yourself to the users hash
+
+users["Faisal"] = [:twitter => "FaisalA"], [:favourite_numbers => [3,6,9]], [:home_town => "Glasgow"], [:pets => {"Tails" => :cat, "Dizzy" => :cat}]
